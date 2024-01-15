32d5e507e67d409ee0e7da51df165523e3b63cdc
ameya-6964 <belvalkar7@gmail.com>
2024-01-16 01:32:31 +0530

    Added Example Of Spread Operator With Arrays and Object

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,5 +1,36 @@
 const App = () => {
-  return <div>hello</div>;
+  /* Shallow Copy/Clone => Bad Practice
+   const arr = [1, 2, 3, 4];
+  const arr2 = arr 
+  */
+
+  // Deep Copy/CLone => Best Practice
+  const arr = [1, 2, 3, 4];
+  const arr2 = [...arr, 5, 6, 7, 8];
+  const state = {
+    firstName: "Ameya",
+    age: 26,
+  };
+
+  const state2 = {
+    ...state,
+    firstName: "Ameya Belvalkar",
+    age: 27,
+  };
+
+  arr2.pop();
+  return (
+    <div>
+      <h1>{arr}</h1>
+      <h1>{arr2}</h1>
+      <h1>
+        {state.firstName},{state.age}
+      </h1>
+      <h1>
+        {state2.firstName},{state2.age}
+      </h1>
+    </div>
+  );
 };
 
 export default App;
