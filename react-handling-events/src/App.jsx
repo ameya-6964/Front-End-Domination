1ab66fda3d770cd02e19a976ecdc6ec014ff987b
ameya-6964 <belvalkar7@gmail.com>
2024-01-31 20:03:47 +0530

    Understood Events

--- react-handling-events/src/App.jsx
+++ react-handling-events/src/App.jsx
@@ -1,13 +1,10 @@
 import Card from "./components/Card";
 
 const App = () => {
-  const names = ["Harsh", "Sarthak", "Ameya", "Tushar", "Suraj"];
   return (
-    <div className="flex justify-between flex-wrap p-10">
-      {names.map((name) => (
-        <Card name={name} key={name} />
-      ))}
-    </div>
+    <>
+      <Card />
+    </>
   );
 };
 
--- react-handling-events/src/components/Card.jsx
+++ react-handling-events/src/components/Card.jsx
@@ -1,22 +1,37 @@
-const Card = ({ name }) => {
+const Card = () => {
+  const data = [
+    {
+      songName: "Mahi Ve",
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit",
+    },
+    {
+      songName: "Hai Junoon",
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit",
+    },
+    {
+      songName: "Knaan Waving Flag",
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit",
+    },
+  ];
   return (
-    <div className="flex justify-between ">
-      <div className="w-52 bg-zinc-100  rounded-md ">
-        <div className=" h-32 bg-zinc-400">
-          <img
-            className="object-cover"
-            src="https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?q=80&w=1796&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Amazon"
-          />
+    <div className="w-full h-screen bg-zinc-300 flex flex-col gap-4 justify-center items-center">
+      {data.map((song, index) => (
+        <div key={index} className="w-60 song px-3 py-2 bg-zinc-100 rounded">
+          <h3 className="font-semibold text-xl">{song.songName}</h3>
+          <p className="text-sm mt-2">{song.description}</p>
+          <button
+            onClick={() => {
+              alert(`Downloading ${song.songName} Song`);
+              setTimeout(() => {
+                alert(`${song.songName} Downloaded`);
+              }, 500);
+            }}
+            className="px-2 py-1 bg-blue-800 text-xs font-semibold text-white rounded-md mt-2"
+          >
+            Download Now
+          </button>
         </div>
-        <div className="w-full px-3 py-4">
-          <h2 className="text-lg font-semibold">{name}</h2>
-          <p className="text-sm mt-3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
-            voluptas.
-          </p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
